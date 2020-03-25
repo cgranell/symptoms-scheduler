637896819644f91b5deff97f6a1ae2a067656092
Carlos Granell <carlos.granell.canut@gmail.com>
2020-03-25 08:25:20 +0100

    plot for the most recent observation data

--- R/analysis.R
+++ R/analysis.R
@@ -2,17 +2,23 @@ library(here)
 library(tidyverse)
 library(lubridate)
 
-file_path <- here("test.csv")
+file_path <- here::here("data-raw", "TestExperiment_24-03-2020T13h15.csv")
 
 data <- read_csv(file_path, col_names = TRUE)
-
 
 data <- 
   data %>%
   mutate(exec_date = as_datetime(exec_timestamp/1000),
          plan_date = as_datetime(planning_timestamp/1000))
 
-all <-
+# plan_date is the driver field to interpet the date. The same date is twice
+
+time_start <- min(data$plan_date)
+time_end <- max(data$plan_date)
+# time_elapsed <- ...
+
+# Delay time is in seconds
+data_formatted <-
   data %>%
   group_by(task) %>%
   arrange(plan_date) %>%
@@ -20,12 +26,12 @@ all <-
          delay = interval(plan_date, exec_date))
 
 
-p_all <- 
-  filter(all, delay >= 60) %>%
+p <- 
+  filter(data_formatted, delay >= 60 & delay <= 61) %>%
   ggplot(aes(x = step, y=delay, group=task, colour = factor(task))) +
   geom_line() +
   labs(x = "time", y = "delay") + 
   theme_bw()
   
-p_all
+p
 
