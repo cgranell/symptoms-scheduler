bc885d3d71df956c526d104d328bd4503c77646a
Carlos Granell <carlos.granell.canut@gmail.com>
2020-03-26 00:21:52 +0100

    add manipulate

--- R/analysis.R
+++ R/analysis.R
@@ -1,6 +1,7 @@
 library(here)
 library(tidyverse)
 library(lubridate)
+library(manipulate)
 
 file_path <- here::here("data-raw", "TestExperiment_24-03-2020T13h15.csv")
 
@@ -15,7 +16,12 @@ data <-
 
 time_start <- min(data$plan_date)
 time_end <- max(data$plan_date)
-# time_elapsed <- ...
+time_elapsed <- interval(time_end,time_start)
+as.duration(time_elapsed) / dminutes(1) # dhours(1), ddays(1)
+as.duration(time_elapsed) / ddays(1)
+
+
+baseline_delay <- 60
 
 # Delay time is in seconds
 data_formatted <-
@@ -23,15 +29,26 @@ data_formatted <-
   group_by(task) %>%
   arrange(plan_date) %>%
   mutate(step = row_number(),
-         delay = interval(plan_date, exec_date))
+         delay = as.duration(interval(plan_date, exec_date)) - baseline_delay)
 
 
-p <- 
-  filter(data_formatted, delay >= 60 & delay <= 61) %>%
-  ggplot(aes(x = step, y=delay, group=task, colour = factor(task))) +
-  geom_line() +
-  labs(x = "time", y = "delay") + 
-  theme_bw()
+data_formatted <- filter(data_formatted, delay >= 0 & delay <= 1)
+
+ylim <- c(min(data_formatted$delay), max(data_formatted$delay))
+ybks <- seq(ylim[1], ylim[2], 0.05)
+xlim <- c(min(data_formatted$step), max(data_formatted$step))
+xbks <- seq(xlim[1], xlim[2], 4)
+
+manipulate(
+  filter(data_formatted, task == task_selected) %>%
+  ggplot(aes(x = step, y=delay)) +
+    geom_line(alpha = 0.6, size = 0.5) +
+    labs(x = "time step", y = "delay [seconds]") + 
+    scale_x_continuous(breaks=xbks, limits=xlim) +
+    scale_y_continuous(breaks=ybks, limits=ylim) +
+    theme_bw(),
+  task_selected = picker("GPS", "dummy", label="Type of task") 
+  )
   
 p
 
