755f0435e267875ea5f738cb7a7472e5531dad2e
Carlos Granell <carlos.granell.canut@gmail.com>
2020-03-27 08:03:41 +0100

    test with pre-experiment dataset

--- R/analysis.R
+++ R/analysis.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(lubridate)
 library(manipulate)
 
-file_path <- here::here("data-raw", "TestExperiment_24-03-2020T13h15.csv")
+file_path <- here::here("data-raw", "PreExperiment_26-03-2020T15h49.csv")
 
 data <- read_csv(file_path, col_names = TRUE)
 
@@ -16,8 +16,8 @@ data <-
 
 time_start <- min(data$plan_date)
 time_end <- max(data$plan_date)
-time_elapsed <- interval(time_end,time_start)
-as.duration(time_elapsed) / dminutes(1) # dhours(1), ddays(1)
+time_elapsed <- interval(time_start, time_end)
+duration <- round(as.duration(time_elapsed) / dminutes(1), 2) # dhours(1), ddays(1)
 
 
 baseline_delay <- 60
@@ -31,12 +31,23 @@ data_formatted <-
          delay = as.duration(interval(plan_date, exec_date)) - baseline_delay)
 
 
-data_formatted <- filter(data_formatted, delay >= 0 & delay <= 1)
+data_formatted <- filter(data_formatted, delay <= 10)
 
 ylim <- c(min(data_formatted$delay), max(data_formatted$delay))
 ybks <- seq(ylim[1], ylim[2], 0.05)
 xlim <- c(min(data_formatted$step), max(data_formatted$step))
 xbks <- seq(xlim[1], xlim[2], 4)
+
+ggplot(data_formatted, aes(x = step, y=delay)) +
+  geom_line(alpha = 0.6, size = 0.5) +
+  labs(title=paste0("Start: ", time_start, " - End: ", time_end),
+       x = "time steps [minutes]", 
+       y = "delay [seconds]") + 
+  scale_x_continuous(breaks=xbks, limits=xlim) +
+  scale_y_continuous(breaks=ybks, limits=ylim) +
+  theme_bw()
+
+
 
 manipulate(
   filter(data_formatted, task == task_selected) %>%
