4e55bc85110348b2774ccd35cb658ec8b9bf1799
Carlos Granell <carlos.granell.canut@gmail.com>
2020-03-28 00:43:17 +0100

    create dual-scale plot

--- R/analysis.R
+++ R/analysis.R
@@ -17,7 +17,7 @@ data <-
 time_start <- min(data$plan_date)
 time_end <- max(data$plan_date)
 time_elapsed <- interval(time_start, time_end)
-duration <- round(as.duration(time_elapsed) / dminutes(1), 2) # dhours(1), ddays(1)
+duration <- round(as.duration(time_elapsed) / dhours(1), 2) # dhours(1), ddays(1)
 
 
 baseline_delay <- 60
@@ -33,19 +33,39 @@ data_formatted <-
 
 data_formatted <- filter(data_formatted, delay <= 10)
 
-ylim <- c(min(data_formatted$delay), max(data_formatted$delay))
-ybks <- seq(ylim[1], ylim[2], 0.05)
+ylim_delay <- c(min(data_formatted$delay), max(data_formatted$delay))
+ybks_delay <- seq(ylim_delay[1], ylim_delay[2], 0.05)
 xlim <- c(min(data_formatted$step), max(data_formatted$step))
-xbks <- seq(xlim[1], xlim[2], 4)
+xbks <- seq(xlim[1], xlim[2], 100)
 
-ggplot(data_formatted, aes(x = step, y=delay)) +
-  geom_line(alpha = 0.6, size = 0.5) +
+ylim_battery <- c(min(data_formatted$battery), max(data_formatted$battery))
+ybks_battery <- seq(0, 100, 10)
+
+# Dual-scale plot
+# https://stackoverflow.com/questions/3099219/ggplot-with-2-y-axes-on-each-side-and-different-scales
+scalefactor <- ylim_delay[2] / ylim_battery[2]  
+
+
+# width = 1600
+ggplot(data_formatted, aes(x = step)) +
+  geom_line(aes(y = delay), alpha = 0.6, size = 0.5) +
+  geom_line(aes(y = battery * scalefactor), color="red") +
+  # geom_smooth(aes(y=delay), method = "lm") +
+  geom_smooth(aes(y=delay), method="loess") +
   labs(title=paste0("Start: ", time_start, " - End: ", time_end),
-       x = "time steps [minutes]", 
-       y = "delay [seconds]") + 
+       x = "time steps [minutes]") + 
   scale_x_continuous(breaks=xbks, limits=xlim) +
-  scale_y_continuous(breaks=ybks, limits=ylim) +
-  theme_bw()
+  scale_y_continuous(name="delay [seconds]", breaks=ybks_delay, limits=ylim_delay, 
+                     sec.axis=sec_axis(~./scalefactor, breaks=ybks_battery, name="battery [%]")) +
+  theme_bw() +
+  theme(
+    axis.line.y.right = element_line(color = "red"), 
+    axis.ticks.y.right = element_line(color = "red"),
+    axis.text.y.right = element_text(color = "red"), 
+    axis.title.y.right = element_text(color = "red")
+    # axis.title.y.left=element_text(color="blue"),
+    # axis.text.y.left=element_text(color="blue"),
+  )
 
 
 
