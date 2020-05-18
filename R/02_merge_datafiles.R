380617d8ae0b189108a59b150a552ed6f180eb91
Carlos Granell <carlos.granell.canut@gmail.com>
2020-05-19 01:34:16 +0200

    explore accurate schedulers

--- R/02_merge_datafiles.R
+++ R/02_merge_datafiles.R
@@ -81,10 +81,12 @@ data_merged <-
   data_merged %>%
   mutate(exec_date = as_datetime(exec_timestamp/1000, tz="Europe/Madrid"),
          plan_date = as_datetime(planning_timestamp/1000, tz="Europe/Madrid"),
+         plan_second = lubridate::second(plan_date),
          plan_minute = lubridate::minute(plan_date),
          plan_hour = lubridate::hour(plan_date),
          plan_day = lubridate::day(plan_date),
          plan_month = lubridate::month(plan_date),
+         exec_second = lubridate::second(exec_date),
          exec_minute = lubridate::minute(exec_date),
          exec_hour = lubridate::hour(exec_date),
          exec_day = lubridate::day(exec_date),
--- R/dual_scale_plot.R
+++ R/dual_scale_plot.R
@@ -53,3 +53,49 @@ dual_scale_plot <- function(selection, plot_title) {
   
     return(p)
 }
+
+one_scale_plot <- function(selection, plot_title) {
+  
+  time_start <- min(selection$plan_date)
+  time_end <- max(selection$plan_date)
+  time_elapsed <- interval(time_start, time_end)
+  duration <- ceiling(as.duration(time_elapsed) / ddays(1))
+  
+  # ylim_delay <- c(min(selection$plan_date_diff), max(selection$plan_date_diff))
+  # ystep <- round((ylim_delay[2] - ylim_delay[1]) / 10, 3) 
+  # ybks_delay <- round(seq(ylim_delay[1], ylim_delay[2], ystep), 3)
+  # 
+  selection %>%
+    ggplot(aes(x = step, group=factor(scheduler), color=factor(scheduler))) +
+    # geom_point(aes(y = delay), alpha = 0.6, size = 0.5) +
+    geom_line(aes(y = plan_date_diff)) +
+    labs(title=plot_title,
+         subtitle=paste0("Start: ", time_start, " - End: ", time_end),
+         x = "Date [2 breaks per day]") + 
+    # scale_x_datetime(breaks = scales::date_breaks("12 hours"),
+    #                  labels = scales::date_format("%d-%m %H:%M", tz="CET"),
+    #                  expand = c(0,0)) +
+    scale_y_continuous(name="delay diff [seconds]") + #, breaks=ybks_delay, limits=ylim_delay) +
+    
+    # guides(color=guide_legend(title="Scheduler",
+    #                           # override.aes=list(fill=NA),
+    #                           nrow=2)) + 
+    # 
+    guides(color=FALSE) +
+    
+    theme(legend.title = element_text(size=8), 
+          legend.position="bottom",
+          legend.direction = "horizontal") +
+    theme_bw() +
+    theme(
+      # axis.line.y.right = element_line(color = "red"), 
+      # axis.ticks.y.right = element_line(color = "red"),
+      # axis.text.y.right = element_text(color = "red"), 
+      # axis.title.y.right = element_text(color = "red"),
+      axis.text.x = element_text(size=7, angle=45)
+      # axis.title.y.left=element_text(color="blue"),
+      # axis.text.y.left=element_text(color="blue"),
+    ) -> p
+  
+  return(p)
+}
